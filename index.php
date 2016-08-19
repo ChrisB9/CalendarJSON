cab51164e7ba9d62b6cdd2a2acd0c93e3582e657
Chris Ben <chris.ben9@gmail.com>
2016-08-19 14:04:59 +0200

    [FIX] changing source of classes

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 header('Content-Type: application/json');
 //error_reporting(E_ALL);
 //ini_set("display_errors", 1);
-require 'CalendarHelper.class.php';
-require 'Day.class.php';
-require 'CalendarDayObject.class.php';
-require 'Calendar.class.php';
+require 'src/CalendarHelper.class.php';
+require 'src/Day.class.php';
+require 'src/CalendarDayObject.class.php';
+require 'src/Calendar.class.php';
 
 $year = $_GET["year"];
 if (!isset($year) || $year == null) {
